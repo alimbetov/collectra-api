d7d31846b087309427cf66c62def911018d6e765
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-09-09 18:24:38 +0500

    feat(file): add FileService application exceptions

---
+++ src/main/java/io/collectra/api/file/application/FileNotFoundException.java
@@ -0,0 +1,9 @@
+package io.collectra.api.file.application;
+
+import java.util.UUID;
+
+public class FileNotFoundException extends RuntimeException {
+    public FileNotFoundException(UUID fileId) {
+        super("File not found: " + fileId);
+    }
+}
