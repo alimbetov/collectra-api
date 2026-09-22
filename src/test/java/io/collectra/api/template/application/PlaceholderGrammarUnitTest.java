26d1f481c82ac2cd5bbafa4ee1c5ca9afccc7c48
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-09-22 21:31:30 +0500

    test(template): cover lowerCamelCase placeholder grammar

--- src/test/java/io/collectra/api/template/application/PlaceholderGrammarUnitTest.java
+++ src/test/java/io/collectra/api/template/application/PlaceholderGrammarUnitTest.java
@@ -12,6 +12,8 @@ class PlaceholderGrammarUnitTest {
     @ValueSource(
             strings = {
                 "customer.name",
+                "customer.displayName",
+                "invoice.invoiceNumber",
                 "document.number",
                 "custom.erp.code",
                 "custom.invoice.manager_name",
@@ -29,6 +31,7 @@ class PlaceholderGrammarUnitTest {
             strings = {
                 "customer",
                 "Customer.Name",
+                "customer.DisplayName",
                 "customer..name",
                 ".customer.name",
                 "customer.name.",
